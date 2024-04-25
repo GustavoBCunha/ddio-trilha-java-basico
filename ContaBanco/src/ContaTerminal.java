85f4c4b29959f29ecae75f40fa1f63198a2c4d27
Gustavo Cunha <gustavo.bcunha@outlook.com>
2024-04-25 12:47:14 -0300

    Alteração Funcional pt.1

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner banco = new Scanner(System.in);
+        
+        System.out.println("Por Favor, digite o seu nome: ");
+        String nome = banco.nextLine();
+
+        System.out.println("Por Favor, digite o número da sua conta: ");
+        int numero = banco.nextInt();
+
+        System.out.println("Por favor, digite o número da sua Agência: ");
+        String agencia = banco.next();
+        
+        System.out.println("Digite seu saldo: ");
+        double saldo = banco.nextDouble();
+
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua Agência é " + agencia + ", conta "+numero+" e seu saldo de "+saldo+" já está disponível para saque." );
+        banco.close();
     }
 }
